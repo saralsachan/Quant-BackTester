5f6caf5e21344d59d88a3bf7eaf8e7a29261aa6a
Saral Sachan <saralsachan06@gmail.com>
2026-04-30 15:08:31 +0530

    Working with close prices

--- src/backtester/data/loader.py
+++ src/backtester/data/loader.py
@@ -17,22 +17,6 @@ def load_ticker(ticker, st, en):
    auto_adjust = True : makes adjusted close happen
    progress = False : just hides the progress bar, cleaner output"""
    
-   
-#loading multiple tickers   
-def load_multiple_tickers(tickers , st, en):
-    #download data for a list of stocks
-    #tickers is a list
-    """return a dictionary: {ticker_name: DataFrame containing OHLCV data for each day}"""
-    
-    result = {} 
-    """we call our prev function for each item of tickers list"""
-    for ticker in tickers:
-       print(f"Loading {ticker} ...")
-       df = load_ticker_with_cache(ticker, st, en) #updated load_ticker with cache function
-       result[ticker] = df
-    
-    return result;    
- 
  
 import os
 
@@ -40,7 +24,9 @@ def load_ticker_with_cache(ticker, start, end):
     """Load a ticker, using a saved file if we've downloaded it before."""
     
     # Make sure the cache folder exists
-    cache_folder = "data/raw"
+   # Get the path to the project root, no matter where the script is called from
+    project_root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+    cache_folder = os.path.join(project_root, "data", "raw")
     os.makedirs(cache_folder, exist_ok=True)
     
     # Build the filename for this ticker
@@ -58,3 +44,36 @@ def load_ticker_with_cache(ticker, start, end):
     df = load_ticker(ticker, start, end)
     df.to_parquet(cache_file)
     return df 
+ 
+"""GETTING CLOSE PRICES in a single DataFrame for different stocks""" 
+def get_close_prices(data_dict):
+   """Take a dictionary of DataFrames and return one DataFrame with close prices.
+    
+    data_dict: dictionary like {"RELIANCE.NS": df, "TCS.NS": df, ...}
+    
+    Returns a DataFrame where each column is one stock's close price.
+    """
+   close_prices = {}
+   for ticker,df in data_dict.items():
+      close_prices[ticker] = df["Close"]
+      
+   return pd.DataFrame(close_prices)   
+    
+    
+    
+   
+#loading multiple tickers   
+def load_multiple_tickers(tickers , st, en):
+    #download data for a list of stocks
+    #tickers is a list
+    """return a dictionary: {ticker_name: DataFrame containing OHLCV data for each day}"""
+    
+    result = {} 
+    """we call our prev function for each item of tickers list"""
+    for ticker in tickers:
+       print(f"Loading {ticker} ...")
+       df = load_ticker_with_cache(ticker, st, en) #updated load_ticker with cache function
+       result[ticker] = df
+    
+    return result;    
+     
