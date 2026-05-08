06ec7318f5934898c559b5413c21f202db5f79f3
Saral Sachan <saralsachan06@gmail.com>
2026-05-08 12:57:35 +0530

    Added total_returns and Annualized returns CAGR metrics

--- src/backtester/metrics/metrics.py
+++ src/backtester/metrics/metrics.py
@@ -13,3 +13,29 @@ def daily_returns(prices):
     The first row will be NaN (no previous day to compare to).
     """
     return prices.pct_change()
+
+
+
+"""Computing the total returns over a period of time"""
+def total_returns(returns):
+    """returns: pandas series contianing returns like 0.1, 0.05, 0.23 etc
+    overall return does not add ,it compounds, use formula (1+r1)*(1+r2)*....-1"""
+    
+    return (1 + returns.dropna()).prod() - 1 #prod() multipies all values, drop NaN values
+
+
+def annualized_returns_CAGR(returns, periods_per_year = 252):
+    """Compute the annualized (per-year) return.
+    
+    returns: pandas Series of daily returns
+    periods_per_year: number of return periods in a year (252 for daily data)
+    
+    Returns a single number — the average yearly return, compounded known as 
+    CAGR(Compounded annual growth rate)."""
+    
+    cleaned = returns.dropna()
+    total_growth = (1 + cleaned).prod()
+    num_years = len(cleaned) / periods_per_year
+    
+    return total_growth**(1/num_years) - 1
+    
